5b3faa790931352ece91ce218f9c619d92195c56
skyline75489 <skyline75489@outlook.com>
2015-01-06 14:28:44 +0800

    Initial Commit

---
+++ Annie/Annie.h
@@ -0,0 +1,19 @@
+//
+//  Annie.h
+//  Annie
+//
+//  Created by skyline on 15/1/6.
+//  Copyright (c) 2015年 skyline. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Annie.
+FOUNDATION_EXPORT double AnnieVersionNumber;
+
+//! Project version string for Annie.
+FOUNDATION_EXPORT const unsigned char AnnieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Annie/PublicHeader.h>
+
+
